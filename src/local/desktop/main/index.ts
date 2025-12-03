7339a470ce0bc93a3cafc666212731ae681e2cb8
bytecode0x <bytecode0x@gmail.com>
2025-12-04 08:56:37 +0900

    20251204; session config

--- src/local/desktop/main/index.ts
+++ src/local/desktop/main/index.ts
@@ -65,6 +65,8 @@ app.on('ready', async function () {
   const eh = getEvHandler()
   eh.addInterface('RENDERER', initMainToRendererEventInterface(eh))
 
+  await session.defaultSession.clearStorageData()
+
   session.defaultSession.webRequest.onBeforeSendHeaders(function (details, cb) {
     details.requestHeaders['User-Agent'] =
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
